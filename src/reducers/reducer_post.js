7d33fc2faa61e171db1e71fb073aa082d77bdcd2
Manmeet Tarun <meetutarun16@gmail.com>
2018-02-18 18:19:39 +0530

    Post is not showing, wtf should i do

--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -4,10 +4,13 @@ import _ from 'lodash';
 export default function(state={},action){
     switch(action.type){
         case FETCH_POSTS:
+            console.log(action.payload.data)
             return _.mapKeys(action.payload.data,'id'); 
         case FETCH_POST:
             const post=action.payload.data;
+            console.log("Prev state: ",state)
             const newState= {...state };
+            console.log("New state: ",newState);
             newState[post.id]=post;
             return newState;
         default:
